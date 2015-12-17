9f820452eabf00e0079ae9cc80cbe4c7c572c583
Luis Capelo <luiscape@gmail.com>
2015-12-17 13:20:54 -0500

    adding unit tests for the classes

---
+++ tests/unit/test_class_dataset.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+Unit tests for the Dataset class.
+
+'''
+import unittest
+
+from scraper.classes.dataset import Dataset
+from scraper.classes.dataverse import Dataverse
+
+class TestClassDataset(unittest.TestCase):
+  '''
+  Performs tests on the Dataverse class.
+
+  '''
+  def setUp(self):
+    self.host = 'dataverse.harvard.edu'
+    self.alias = 'IFPRI'
+    self.dataset_id = 57641
+    self.dataset_id_error = 23748295482569256
+    self.dataverse = Dataverse(host=self.host, alias=self.alias)
+
+  def test_dataset_info_is_returned_correctly(self):
+    '''
+    classes.dataset: Tests that the dataset information is returned correctly.
+
+    '''
+    result = Dataset(self.dataset_id).info()
+    self.assertIs(type(result), type({}))
+
+  def test_dataset_info_returns_error_if_doesnt_exist(self):
+    '''
+    classes.dataset: Tests that dataset returns an error message if it doesn't exist.
+
+    '''
+    result = Dataset(self.dataset_id_error).info()
+    self.assertTrue(result['status'] == 'ERROR')
---
+++ tests/unit/test_class_dataverse.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+Unit tests for the Dataverse class.
+
+'''
+import unittest
+
+from scraper.classes.dataverse import Dataverse
+
+class TestClassDataverse(unittest.TestCase):
+  '''
+  Performs tests on the Dataverse class.
+
+  '''
+  def setUp(self):
+    self.host = 'dataverse.harvard.edu'
+    self.alias = 'IFPRI'
+
+  def test_info_is_collected_if_host_and_alias_are_correct(self):
+    '''
+    classes.dataverse: Tests that the Dataverse class returns info if initialized correctly.
+
+    '''
+    result = Dataverse(host=self.host, alias=self.alias).info()
+    self.assertIs(type(result), type({}))
+
+  # def test_content_of_dataverse_returns_correctly(self):
+  #   '''
+  #   classes.dataverse: Tests that the content of a dataverse is returned correctly.
+
+  #   '''
+  #   result = Dataverse(host=self.host, alias=self.alias).contents()
+  #   self.assertIs(type(result), type([]))
